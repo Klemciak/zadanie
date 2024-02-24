daea0cabb3853100f9c4e4b439e6cfb440f1277a
Klemciak <klemcio@interia.pl>
2024-02-24 22:07:00 +0100

    update1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./layout/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App basename={process.env.PUBLIC_URL} />
+  <React.StrictMode basename={process.env.PUBLIC_URL}>
+    <App />
   </React.StrictMode>
 );
